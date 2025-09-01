e2499b72235259330bbd7c749a690f251a88427c
Stephen <stephen@klu.ai>
2025-09-01 10:23:33 +0700

    aarch64

---
+++ sys/arch/aarch64/include/intr.h
@@ -0,0 +1,8 @@
+/*
+ * intr.h
+ * TODO: Provide AArch64-specific interrupt definitions and interfaces.
+ */
+#ifndef _AARCH64_INTR_H_
+#define _AARCH64_INTR_H_
+/* TODO: implement for AArch64 */
+#endif /* _AARCH64_INTR_H_ */
---
+++ sys/arch/aarch64/include/pmap.h
@@ -0,0 +1,8 @@
+/*
+ * pmap.h
+ * TODO: Provide AArch64-specific pmap interface and definitions.
+ */
+#ifndef _AARCH64_PMAP_H_
+#define _AARCH64_PMAP_H_
+/* TODO: implement for AArch64 */
+#endif /* _AARCH64_PMAP_H_ */
